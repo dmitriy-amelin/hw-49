aeebdacc8490b1f7a67f0f710fe9f3c95f938793
Reey <trommxxx@mail.ru>
2021-03-11 16:15:25 +0600

    Созданы модели задачи, статуса и типа

--- hw_49/issue_tracker/models.py
+++ hw_49/issue_tracker/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Status(models.Model):
+    name_of_status = models.CharField(max_length=100, null=False, blank=False, verbose_name='Статус')
+
+
+class Type(models.Model):
+    name_of_type = models.CharField(max_length=100, null=False, blank=False, verbose_name='Тип')
+
+
+class Task(models.Model):
+    summary = models.TextField(verbose_name='Краткое описание')
+    description = models.TextField(blank=True, verbose_name='Полное описание')
+    status = models.ForeignKey('issue_tracker.Status',
+                               on_delete=models.CASCADE,
+                               related_name='status',
+                               verbose_name='Статус',
+                               null=False,
+                               blank=False
+                               )
+    type = models.ForeignKey('issue_tracker.Type',
+                             on_delete=models.CASCADE,
+                             related_name='type',
+                             verbose_name='Тип',
+                             null=False,
+                             blank=False
+                             )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
