188bd912e8ae368733442da8e60c28ae93cd6e10
Reey <trommxxx@mail.ru>
2021-03-11 18:19:04 +0600

    Создана страница просмотра одной задачи

--- hw_49/issue_tracker/urls.py
+++ hw_49/issue_tracker/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from issue_tracker.views import (
     IndexView,
-#     task_view,
+    TaskView,
 #     task_add,
 #     task_update,
 #     task_delete,
@@ -10,7 +10,7 @@ from issue_tracker.views import (
 
 urlpatterns = [
     path('', IndexView.as_view(), name='task-list'),
-    # path('task/<int:pk>/', task_view, name='task-view'),
+    path('task/<int:pk>/', TaskView.as_view() , name='task-view'),
     # path('task/add/', task_add, name='task-add'),
     # path('<int:pk>/update/', task_update, name='task-update'),
     # path('<int:pk>/delete/', task_delete, name='task-delete'),
