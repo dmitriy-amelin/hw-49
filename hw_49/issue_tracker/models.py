2dfc4ce6f335a2fb4009eb5c6fb41fcc6bf7dccd
Reey <trommxxx@mail.ru>
2021-03-11 16:35:12 +0600

    Обновлены модели, добавлен класс Meta. обновлены и применены миграции

--- hw_49/issue_tracker/models.py
+++ hw_49/issue_tracker/models.py
@@ -4,9 +4,18 @@ from django.db import models
 class Status(models.Model):
     name_of_status = models.CharField(max_length=100, null=False, blank=False, verbose_name='Статус')
 
+    class Meta:
+        db_table = 'status'
+        verbose_name = 'Статус'
+        verbose_name_plural = 'Статусы'
 
 class Type(models.Model):
     name_of_type = models.CharField(max_length=100, null=False, blank=False, verbose_name='Тип')
+
+    class Meta:
+        db_table = 'type'
+        verbose_name = 'Тип'
+        verbose_name_plural = 'Типы'
 
 
 class Task(models.Model):
@@ -28,3 +37,8 @@ class Task(models.Model):
                              )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'task'
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
