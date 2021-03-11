267beaf010d2b412c5950e5f378bb2e629aba236
Reey <trommxxx@mail.ru>
2021-03-11 18:10:34 +0600

    Создан базовый шаблон

--- hw_49/issue_tracker/views.py
+++ hw_49/issue_tracker/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import View, TemplateView, RedirectView
 
-# Create your views here.
+from issue_tracker.models import Type, Task, Status
+# from issue_tracker.forms import TaskForm, TaskDeleteForm
+
+
+class IndexView(TemplateView):
+
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        kwargs['tasks'] = Task.objects.all()
+        return super().get_context_data(**kwargs)
