79496053e1cedd937d1ea832c047ef786b31ba2a
Reey <trommxxx@mail.ru>
2021-03-11 17:22:45 +0600

    Модели зарегистрированы в админке

--- hw_49/issue_tracker/admin.py
+++ hw_49/issue_tracker/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from issue_tracker.models import Task, Type, Status
 
-# Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'summary', 'description', 'status', 'type', 'created_at', 'updated_at']
+    list_filter = ['status', 'type']
+    search_fields = ['summary', 'description', 'status', 'type']
+    fields = ['id', 'summary', 'description', 'status', 'type']
+    readonly_fields = ['created_at', 'updated_at', 'id']
+
+
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ['name_of_status']
+    fields = ['name_of_status']
+
+
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ['name_of_type']
+    fields = ['name_of_type']
+
+
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Status, StatusAdmin)
+admin.site.register(Type, TypeAdmin)
