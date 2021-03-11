0cac3eedd2b9918909121252fd41eeb5cc8234e1
Reey <trommxxx@mail.ru>
2021-03-11 18:10:48 +0600

    Создана главная страница

---
+++ hw_49/issue_tracker/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from issue_tracker.views import (
+    IndexView,
+#     task_view,
+#     task_add,
+#     task_update,
+#     task_delete,
+)
+
+urlpatterns = [
+    path('', IndexView.as_view(), name='task-list'),
+    # path('task/<int:pk>/', task_view, name='task-view'),
+    # path('task/add/', task_add, name='task-add'),
+    # path('<int:pk>/update/', task_update, name='task-update'),
+    # path('<int:pk>/delete/', task_delete, name='task-delete'),
+]
